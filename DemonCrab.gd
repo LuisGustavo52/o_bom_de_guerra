14b3c271f455eb12d2a19e90623e84e1fb16234a
Luís Gustavo Teodoro da Silva <luis.gustavo52@gmail.com>
2023-11-22 20:41:07 -0300

    algoritmo de desabilitação das colisões do caranguiejo prontos

--- DemonCrab.gd
+++ DemonCrab.gd
@@ -1,6 +1,8 @@
 extends KinematicBody2D
 
 var hit = 0
+onready var colisao_frente = $mao/hit_box_crab_frente
+onready var colisao_atras = $mao/hit_box_crab_atras
 
 func hit():
 	hit = hit + 1
@@ -9,10 +11,11 @@ func hit():
 onready var anim = $AnimatedSprite
 
 func _physics_process(delta):		
-	pass
-	#print (anim.get_animation())
-	#if (anim.get_frame() == 1):
-		
-
+	if (anim.get_animation() == "ataque_cima" ):
+		if (anim.get_frame() == 2):
+			colisao_frente.set_deferred("disabled", false)
+	elif (anim.get_animation() == "ataque_baixo"):
+		if (anim.get_frame() == 2):
+			colisao_frente.set_deferred("disabled", false)
 
 
